04e4196cacd81a3a9440b1f77ba89334d1cef617
Samantha González <samujm76@gmail.com>
2022-09-04 21:32:30 -0500

    Funcionamiento js

---
+++ script.js
@@ -0,0 +1,54 @@
+const smallCups = document.querySelectorAll('.cup-small')
+const listers = document.getElementById('liters')
+const pecentage = document.getElementById('percentage')
+const remained = document.getElementById('remained')
+
+updateBigCup()
+
+smallCups.forEach((cup, idx)=>{
+    // console.log(idx)
+    cup.addEventListener('click',()=> highlightCups(idx))
+})
+
+function highlightCups(idx){
+    // console.log(idx)
+
+    if(smallCups[idx].classList.contains('full') && !smallCups[idx].nextElementSibling.classList.contains('full')){
+        idx--
+    }
+
+    smallCups.forEach((cup, idx2)=>{
+        if(idx2 <= idx){
+            cup.classList.add('full')
+        }else{
+            cup.classList.remove('full')
+        }
+    })
+
+    updateBigCup()
+}
+
+function updateBigCup(){
+    const fullCups = document.querySelectorAll('.cup-small.full').length
+    const totalCups = smallCups.length
+    // console.log(totalCups)
+
+    if (fullCups === 0) {
+        pecentage.style.visibility = 'hidden'
+        percentage.style.height = 0
+    } else {
+        percentage.style.visibility = 'visible'
+        percentage.style.height = `${fullCups / totalCups * 330}px`
+    }
+
+    percentage.innerText = `${fullCups / totalCups * 100}%`
+
+    if (fullCups === totalCups) {
+        remained.style.visibility = 'hidden'
+        remained.style.height = 0
+    } else {
+        remained.style.visibility = 'visible'
+        listers.innerText = `${2 - (250 * fullCups /1000)}`
+    }
+
+}
